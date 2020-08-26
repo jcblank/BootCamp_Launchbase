7605c728d0400e9cfe6a15f88d430047a3628187
João Carlos Rodrigues <jcarlos.x@gmail.com>
2020-08-25 23:05:58 -0300

    Exercício array JS

--- introducao_programacao/vetores.js
+++ introducao_programacao/vetores.js
@@ -1,27 +1,57 @@
 
 // Criar programa para calcular a média
-// das notas dos alunos
-const alunos = [
+// das duas turmas e verificar qual é a maior
+
+const turmaA = [
     {
         nome: "João",
-        nota: 9.2
+        nota: 10
     },
     {
         nome: "Maria",
-        nota: 5.8
+        nota: 9.8
     },
     {
         nome: "Antonio",
-        nota: 5.6
+        nota: 8.6
     }
 ]
 
+const turmaB = [
+    {
+        nome: "Antônio",
+        nota: 4.6
+    },
+    {
+        nome: "Tião",
+        nota: 5.7
+    },
+    {
+        nome: "Joaquina",
+        nota: 3.7
+    }
+]
 
-const media = (alunos[0].nota, alunos[1].nota, alunos[2].nota) / 3
-
-
-if (media >=5) {
-    console.log(`A média geral da turma ${media} está boa!`)
-} else {
-    console.log (`A turma precisa estudar mais, pois a média ${media} está baixa.`)
+// Função para efetuar o cálculo da média
+// das notas
+function calculaMedia (alunos) {
+    return (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
 }
+
+// Resultado das médias
+const media1 = calculaMedia(turmaA)
+const media2 = calculaMedia(turmaB)
+
+// Função para efetuar o teste da média e
+//  retornar a mensagem.
+function mensagem(media, turma) {
+    if (media >= 5 ) {
+        console.log (`A Turma ${turma} está de parabéns pois teve uma média de ${media.toFixed(2)}, ficando acima do esperado!`)
+    } else {
+        console.log (`A Turma ${turma} precisa estudar mais pois sua média foi somente de ${media.toFixed(2)}`)
+    }
+
+}
+// Chamando a função e passando a mensagem.
+mensagem(media1, 'TurmaA')
+mensagem(media2, 'TurmaB')
