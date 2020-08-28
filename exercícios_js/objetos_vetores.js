389b2641abde54047c6db3e6812e334ebc08a45e
jcblank <jcarlos.x@gmail.com>
2020-08-28 10:48:10 -0300

    Arquivo deletado

--- exercícios_js/objetos_vetores.js
+++
@@ -1,9 +0,0 @@
-const usuario = {
-    nome: "Diego",
-    empresa: {
-      nome: "Rocketseat"
-    }
-  };
-
-  console.log(`O nome do usuário é ${usuario.nome}`)
-  console.log(`A empresa ${usuario.empresa.nome} está localizada em Rua Guilherme Gembala, 260`)
