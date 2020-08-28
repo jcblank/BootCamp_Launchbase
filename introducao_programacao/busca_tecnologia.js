c43deb82765d2d8690cae9c380275d87a2681320
jcblank <jcarlos.x@gmail.com>
2020-08-28 11:57:39 -0300

    inicio exercicio busca_tecnologia

---
+++ introducao_programacao/busca_tecnologia.js
@@ -0,0 +1,26 @@
+// Crie um programa que armazena um array de usuários (objetos), 
+// cada usuário tem um nome e suas tecnologias (novo array)
+
+
+// Objeto com usuários
+const users = [
+    {
+        nome: "Carlos",
+        tecnologias: ["HTML","CSS"]
+    },
+    {
+        nome: "Elliot",
+        tecnologias: ["Linux","Phyton"]
+    },
+    {
+        nome: "Maria",
+        tecnologias: ["JS","Node"]
+    }
+];
+
+
+function cssCheck(users) {
+    for(let i = 0; i < users.tecnologias; i++) {
+        
+    }
+}
