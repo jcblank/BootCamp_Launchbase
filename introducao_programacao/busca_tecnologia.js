eab35b3707b9a9546a6085ee2d66d81bf8ed5dde
jcblank <jcarlos.x@gmail.com>
2020-08-31 12:10:57 -0300

    desafio busca tecnologia concluido

--- introducao_programacao/busca_tecnologia.js
+++ introducao_programacao/busca_tecnologia.js
@@ -1,6 +1,6 @@
-// Crie um programa que armazena um array de usuários (objetos), 
-// cada usuário tem um nome e suas tecnologias (novo array)
-
+// Crie uma função que recebe os dados de um objeto de usuário 
+// e retorna SE o usuário trabalha com CSS ou não. 
+// Essa função deve retornar um boolean true/false.
 
 // Objeto com usuários
 const users = [
@@ -18,9 +18,25 @@ const users = [
     }
 ];
 
-
-function cssCheck(users) {
-    for(let i = 0; i < users.tecnologias; i++) {
-        
+// Função para validar se o usuário trabalha com CSS
+function checkCss(users) {
+    for(let tecnologia of users.tecnologias) {
+        if(tecnologia == 'CSS') 
+        return true
     }
 }
+
+// For para percorrer a lista de usuários, testar
+// e imprimir em tela se ele trabalha ou não com CSS
+for (let i = 0; i < users.length; i++) {
+    const userHaveCss = checkCss(users[i]);
+    
+    if(userHaveCss) {
+        console.log(`${users[i].nome}, trabalha com CSS!`)
+    } else {
+        console.log(`${users[i].nome},não trabalham com CSS, mas trabalham com ${users[i].tecnologias}!`)
+    }
+} 
+ 
+
+
