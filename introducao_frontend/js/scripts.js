dcc5fb2efe595965a291fd5d6e71be3880eb1da0
jcblank <jcarlos.x@gmail.com>
2020-09-16 11:09:00 -0300

    commit geral

--- introducao_frontend/js/scripts.js
+++ introducao_frontend/js/scripts.js
@@ -4,16 +4,20 @@ const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
 
 // for para adicionar ao click a classe
-// active 
+// active e incluir id do src
 for (let card of cards) {
     card.addEventListener("click", function() {
+        const videoId = card.getAttribute("id");
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`        
     })
 }
 
 // Ao click do botão "close-modal"
-// remover a classe active
+// remover a classe active e 
+// remover src do iframe 
 document.querySelector('.close-modal').addEventListener("click", function() {
     modalOverlay.classList.remove("active") 
+    modalOverlay.querySelector("iframe").src =("")
 })
 
