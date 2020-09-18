8f28c5aaea4dd95e16d118d8d987d24abfb419fb
jcblank <jcarlos.x@gmail.com>
2020-09-18 11:08:41 -0300

    Criado funçao para fechamento do modal

--- introducao_frontend/js/scripts.js
+++ introducao_frontend/js/scripts.js
@@ -2,10 +2,24 @@
 // css
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
-
 const modalContent = document.querySelector('.modal-mid');
 const contents = document.querySelectorAll('.content');
 
+// Função para remover a classe "active" do modal
+function modal (close,remove) { 
+    document.querySelector(close).addEventListener("click", function() {
+        remove.classList.remove("active") 
+    })
+};
+
+for (let content of contents) {
+    content.addEventListener("click", function() {
+        const blogID = content.getAttribute("id");
+        modalContent.classList.add('active')
+        modalContent.querySelector("iframe").src = `https://blog.rocketseat.com.br/${blogID}`
+    })
+    modal('.modal-close',modalContent);
+};
 
 // for para adicionar ao click a classe
 // active e incluir id do src
@@ -15,18 +29,6 @@ for (let card of cards) {
         modalOverlay.classList.add('active')
         modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`        
     })
+    modal('.close-modal',modalOverlay);
 };
-
-// Ao click do botão "close-modal"
-// remover a classe active e 
-// remover src do iframe 
-document.querySelector('.close-modal').addEventListener("click", function() {
-    modalOverlay.classList.remove("active") 
-    modalOverlay.querySelector("iframe").src =("")
-});
-
-for (let content of contents) {
-    content.addEventListener("click", function() {
-        modalContent.classList.add('active')
-    })
-}
+ 
