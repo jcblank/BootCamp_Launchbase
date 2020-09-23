48a0697265860fd219707bdaf2abd4dbfb877a42
jcblank <jcarlos.x@gmail.com>
2020-09-22 21:58:11 -0300

    Incluido logica para maximizar mmodal

--- introducao_frontend/js/scripts.js
+++ introducao_frontend/js/scripts.js
@@ -4,6 +4,8 @@ const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
 const modalContent = document.querySelector('.modal-mid');
 const contents = document.querySelectorAll('.content');
+const maximize = document.querySelector('.modal-maximize');
+const modalInner = document.querySelector('.inner');
 
 // Função para remover a classe "active" do modal
 function modal (close,remove) { 
@@ -12,14 +14,13 @@ function modal (close,remove) {
     })
 };
 
-for (let content of contents) {
-    content.addEventListener("click", function() {
-        const blogID = content.getAttribute("id");
-        modalContent.classList.add('active')
-        modalContent.querySelector("iframe").src = `https://blog.rocketseat.com.br/${blogID}`
+function maximizar () {
+    maximize.addEventListener("click", function() {
+        modalInner.classList.toggle('maximize');
     })
-    modal('.modal-close',modalContent);
 };
+
+
 
 // for para adicionar ao click a classe
 // active e incluir id do src
@@ -30,5 +31,17 @@ for (let card of cards) {
         modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`        
     })
     modal('.close-modal',modalOverlay);
+    
 };
  
+for (let content of contents) {
+    content.addEventListener("click", function() {
+        const blogID = content.getAttribute("id");
+        modalContent.classList.add('active')
+        modalContent.querySelector("iframe").src = `https://blog.rocketseat.com.br/${blogID}`
+    })
+    modal('.modal-close',modalContent);
+};
+
+maximizar();
+
