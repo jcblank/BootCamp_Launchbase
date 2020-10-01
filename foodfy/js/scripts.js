538e769a1632ee6a512329a75b7927805f102c9f
jcblank <jcarlos.x@gmail.com>
2020-10-01 10:25:22 -0300

    inserido logica para pegar dados do css pra dentro do modal

--- foodfy/js/scripts.js
+++ foodfy/js/scripts.js
@@ -13,9 +13,13 @@ function modal (close,remove) {
 
 for (let card of cards) {
     card.addEventListener("click", function() {
-        const videoId = card.getAttribute("h2");
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector("h2") = `${videoId}`.innerHTML;     
+        const modalImg = card.querySelector("img").src;
+        const modalH2 = card.querySelector("h2");
+        const modalP = card.querySelector("p");
+        modalOverlay.classList.add('active');
+        modalOverlay.querySelector("img").src = `${modalImg}`;
+        modalOverlay.querySelector("h2").innerHTML = modalH2.innerHTML;
+        modalOverlay.querySelector("p").innerHTML = modalP.innerHTML;
     })
     modal('.close-modal',modalOverlay);
 };
