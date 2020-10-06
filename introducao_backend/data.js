70537ff65e4fe06f042d681732f4b5943eae18c8
jcblank <jcarlos.x@gmail.com>
2020-10-06 16:20:24 -0300

    adicionado arquivo dos dados

---
+++ introducao_backend/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "2alg7MQ6_sI",
+        title: "Git na prática",
+        duration: "57 min",
+        price: "free"
+    },
+    {
+        id: "-j7vLmBMsEU",
+        title: "Deploy de uma applicação na prática",
+        duration: "35 min",
+        price: "R$29,90"
+    },
+    {
+        id: "BN_8bCfVp88",
+        title: "[API NodeJS + Express + Mongo] Estrutura e cadastro",
+        duration: "20 horas",
+        price: "R$139,90"
+    },
+    {
+        id: "sYe4r8WXGQg",
+        title: "Controlando autenticação em rotas no ReactJS",
+        duration: "57 min",
+        price: "free"
+    },
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS",
+        duration: "57 min",
+        price: "free"
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "Array: Higher Order Functions",
+        duration: "57 min",
+        price: "free"
+    }
+]
