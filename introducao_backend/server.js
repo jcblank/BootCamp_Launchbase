a2d3dd2c976d9880064f7a554358c2f708786110
jcblank <jcarlos.x@gmail.com>
2020-10-26 10:12:37 -0300

    criado pagina video e refatoracao

--- introducao_backend/server.js
+++ introducao_backend/server.js
@@ -30,14 +30,25 @@ server.get("/", function(req, res) {
 
     return res.render("about", {about})
 })
-
+ 
 server.get("/portfolio", function(req, res) {
     return res.render("portfolio", { items: videos })
 })
 
 server.get("/video", function(req,res) {
     const id = req.query.id
-    res.send(id)
+    
+    const video = videos.find(function(video){
+        if (video.id == id) {
+            return true;
+        }
+    })
+
+    if (!video) {
+        return res.send("Video not found!")
+    }
+
+    return res.render("video", {item: video})
 })
 
 
